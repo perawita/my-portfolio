4e92c3d96e87168fe7036874e04e62f17efd3692
perawitayasa <perawitayasa@gmail.com>
2024-06-22 20:13:27 +0000

    add hoby

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>ida bagus perawita yasa</p>
+      <p>saya suka olahraga</p>
     </main>
   );
 }
