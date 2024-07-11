2bd7cd24aee5496c694acf90fea6c98e40571b67
perawitayasa <perawitayasa@gmail.com>
2024-07-11 20:38:04 +0000

    add new navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import RootLayout from "@/app/layout";
+import { Navbars } from "@/app/__components/navbar";
 import { Background_Globe } from "@/app/__components/background_globe";
 
 export default function Home() {
   return (
     <RootLayout>
+        <Navbars />
         <Background_Globe />
     </RootLayout>
   );
