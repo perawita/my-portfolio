1bad18414db4e0486781094bcf39d5ee3d1e133e
perawita <perawitayasa@gmail.com>
2024-09-20 01:25:44 +0800

    tambah class .text-white

--- app/__components/background.tsx
+++ app/__components/background.tsx
@@ -35,7 +35,7 @@ export function Background() {
                             semenjak 2021, dan memulai sebagai freelance aktif dari tahun 2024
                         </p>
 
-                        <a href="/services" className="inline-flex h-12 mt-4 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium md:text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                        <a href="/services" className="inline-flex h-12 mt-4 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white">
                             Ingin mencoba jasa saya ?
                         </a>
                     </motion.div>
